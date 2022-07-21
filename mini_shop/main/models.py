d8acd6ece72cefed24a306fed0e99fea3b0ad218
Alexey Shamota <ashamota@GMCS.local>
2022-07-21 20:17:13 +0300

    Tasks 1, 2

---
+++ mini_shop/main/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Organization(models.Model):
+    name = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+
+    def __str__(self):
+        return f"Название организации: {self.name}\nОписание: {self.description}"
+
+
+class Shop(models.Model):
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    name = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+    address = models.TextField("Адрес")
+    index = models.IntegerField("Индекс")
+    is_deleted = models.BooleanField("Удалено ли")
+
+    def __str__(self):
+        return f"Название магазина: {self.name}\nОписание: {self.description}"
