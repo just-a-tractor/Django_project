647b8a360651c7d738f6b8de1c00289890bfce47
ashamota <alex.gif2003@gmail.com>
2022-07-24 14:53:57 +0300

    Bugfix

--- mini_shop/main/urls.py
+++ mini_shop/main/urls.py
@@ -1,12 +1,8 @@
-from django.urls import include, path
 from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'organizations', views.OrganizationViewSet)
-router.register(r'shops', views.ShopViewSet)
+router.register(r'api/organizations', views.OrganizationViewSet)
+router.register(r'api/shops', views.ShopViewSet)
 
-urlpatterns = [
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+urlpatterns = router.urls
