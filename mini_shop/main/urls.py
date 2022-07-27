8f054e2885f640eb9acc1ca82f1b71e12e4ca985
ashamota <alex.gif2003@gmail.com>
2022-07-27 16:52:28 +0300

    Task 7 done

--- mini_shop/main/urls.py
+++ mini_shop/main/urls.py
@@ -1,8 +1,18 @@
+from django.urls import path, include
 from rest_framework import routers
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 router = routers.DefaultRouter()
 router.register(r'api/organizations', views.OrganizationViewSet)
 router.register(r'api/shops', views.ShopViewSet)
+# router.register(r'api/token', TokenObtainPairView, basename='token_obtain_pair')
+# router.register(r'api/token/refresh', TokenRefreshView, basename='token_refresh')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path(r'api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(r'api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(r'api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
