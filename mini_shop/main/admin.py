c59ba2b10b90d1fdf06bb91323546dd5526327d2
ashamota <alex.gif2003@gmail.com>
2022-08-03 19:51:50 +0300

    Task 12 done

--- mini_shop/main/admin.py
+++ mini_shop/main/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Organization, Shop
 
-admin.site.register(Organization)
-admin.site.register(Shop)
+
+class ShopInline(admin.TabularInline):
+    model = Shop
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    inlines = [
+        ShopInline,
+    ]
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
